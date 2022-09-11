a98e890b1d6610f96430ba87d35713beee36afa3
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-11 20:34:10 +0300

    add having in query

--- Students Registeration/SQL/number of majoring students in department with more than one majoring student.sql
+++ Students Registeration/SQL/number of majoring students in department with more than one majoring student.sql
@@ -1,8 +1,6 @@
 -- number of majoring students in department with more than one majoring student
 
-select department_name, majoring_students from(
 select department.name as department_name, count(majorsin.student_id) as majoring_students from department, majorsin
 where majorsin.dept_id = department.id
 group by department.id
-) as sub
-where majoring_students > 1
+having majoring_students > 1
